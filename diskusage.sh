25359eb24bb3e3620149bb72c7935150eb48e80e
Solid-Smoke <PABLO.CASCANTETORRES@ucr.ac.cr>
2025-06-15 11:59:53 -0600

    diskusage v1.0.0

---
+++ diskusage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+logfile="$HOME/.diskusage.txt"
+
+df -h -BM /
+
+if [ ! -f "$logfile" ]; then
+  (echo -ne "Timestamp\t    " && df -h -BM /) | head -n 1 > "$logfile"
+fi
+
+echo "$(date +%F\ %T) $(df -h -BM / | tail -n 1)" >> "$logfile"
+echo "See log with: cat $logfile"
